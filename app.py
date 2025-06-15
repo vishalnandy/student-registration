81c3a6ca4da73483c2907d00084686d9b798f7c8
Nandy <vishal.nandy@accenture.com>
2025-06-15 23:20:12 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    age = db.Column(db.Integer, nullable=False)
+
+@app.route('/')
+def index():
+    students = Student.query.all()
+    return render_template('register.html', students=students)
+
+@app.route('/register', methods=['POST'])
+def register():
+    new_student = Student(
+        name=request.form['name'],
+        email=request.form['email'],
+        age=int(request.form['age'])
+    )
+    db.session.add(new_student)
+    db.session.commit()
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
